9db7cefe8a044c677cab26d65c502ed4252b1348
ginni0002 <sekhonginni0002@gmail.com>
2026-09-25 14:34:34 -0600

    Added color to Shape class

--- app/src/main/java/com/example/atomic_cmput_301/Shape.kt
+++ app/src/main/java/com/example/atomic_cmput_301/Shape.kt
@@ -1,6 +1,8 @@
 package com.example.atomic_cmput_301
 
-class Shape {
-    val x : Int = 0
-    val y: Int = 0
+abstract class Shape {
+    open val x: Int = 0
+    open val y: Int = 0
+    val color: String = "blue"
+    
 }
